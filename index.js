c81b070be6414cac0cfb8ef79e04116bd29c2109
tobiasraab <64197026+tobiasraab@users.noreply.github.com>
2023-05-01 17:24:20 +0200

    Finished Challenge

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
+const multer = require('multer')
 var cors = require('cors');
+const fs = require('fs')
+
 require('dotenv').config()
+
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -12,6 +17,26 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (req.file) {
+    const fileData = req.file
+    console.log(`Received uploaded File: ${JSON.stringify(fileData)}`)
+    res.json({
+      name: fileData.originalname,
+      type: fileData.mimetype,
+      size: fileData.size
+    })
+    fs.unlink('./uploads/' + fileData.filename, (error) => {
+      if (error) {
+        throw (error)
+      }
+    })
+  } else {
+    res.json({
+      error: 'no file selected'
+    })
+  }
+})
 
 
 const port = process.env.PORT || 3000;
